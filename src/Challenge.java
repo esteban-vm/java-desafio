92f9dbb01ea9500355c752ef5f581417150d39bc
Esteban V.M. <estebanvm90@outlook.com>
2025-04-08 12:14:23 -0300

    implementa los cálculos

--- src/Challenge.java
+++ src/Challenge.java
@@ -18,7 +18,7 @@ public class Challenge {
 
         String menu = """
                 
-                Escriba el número de la opción deseada:
+                ** Escriba el número de la opción deseada: **
                 1. Consular saldo.
                 2. Retirar.
                 3. Depositar.
@@ -29,6 +29,46 @@ public class Challenge {
         while (option != 9) {
             System.out.println(menu);
             option = scanner.nextInt();
+
+            switch (option) {
+                case 1: {
+                    System.out.println("El saldo actualizado es: " + balance + "$");
+                    break;
+                }
+
+                case 2: {
+                    System.out.println("¿Qué monto desea retirar?");
+                    double withdrawal = scanner.nextDouble();
+
+                    if (withdrawal > balance) {
+                        System.out.println("Saldo insuficiente");
+                    } else {
+                        balance -= withdrawal;
+                        System.out.println("El saldo actualizado es: " + balance + "$");
+                    }
+
+                    break;
+                }
+
+                case 3: {
+                    System.out.println("¿Qué monto desea depositar?");
+                    double deposit = scanner.nextDouble();
+                    balance += deposit;
+                    System.out.println("El saldo actualizado es: " + balance + "$");
+                    break;
+                }
+
+                case 9: {
+                    System.out.println("Gracias por utilizar nuestros servicios");
+                    System.out.println("Saliendo del programa…");
+                    scanner.close();
+                    break;
+                }
+
+                default: {
+                    System.out.println("Opción no válida");
+                }
+            }
         }
     }
 }
