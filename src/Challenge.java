b859120328f494d6f562665c100f2fc78c6955f9
Esteban V.M. <estebanvm90@outlook.com>
2025-04-08 11:22:48 -0300

    inicializa los valores de la cuenta

---
+++ src/Challenge.java
@@ -0,0 +1,12 @@
+public class Challenge {
+    public static void main(String[] args) {
+        String customer = "Tony Stark";
+        String accountType = "corriente";
+        double balance = 1599.99;
+
+        System.out.println("********************************");
+        System.out.println("\nNombre del cliente:" + customer);
+        System.out.println("Tipo de cuenta:" + accountType);
+        System.out.println("Saldo disponible:" + balance + "$");
+    }
+}
